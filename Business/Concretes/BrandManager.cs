8b6996cf5d2411b44f5446bde87ceed6b5d2a72f
Melike Yılmaz <melike.yilmaz@ailevecalisma.gov.tr>
2022-02-28 10:12:06 +0300

    Add project files.

---
+++ Business/Concretes/BrandManager.cs
@@ -0,0 +1,43 @@
+﻿using Business.Abstracts;
+using DataAccess.Abstracts;
+using Entities.Concretes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concretes
+{
+    public class BrandManager : IBrandService
+
+    {
+
+        IBrandDal _brandDal;
+
+        public BrandManager(IBrandDal brandDal)
+        {
+            _brandDal = brandDal;
+        }
+
+        public void Add(Brand brand)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(Brand brand)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Brand> GetAll()
+        {
+          return   _brandDal.GetList();
+        }
+
+        public void Update(Brand brand)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ MVCApp/Controllers/BrandController.cs
@@ -0,0 +1,24 @@
+﻿using Business.Abstracts;
+using Business.Concretes;
+using DataAccess.Concretes;
+using DataAccess.Concretes.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MVCApp.Controllers
+{
+    public class BrandController : Controller
+    {
+        IBrandService _brandService;
+
+        public BrandController(IBrandService brandService)
+        {
+            this._brandService = brandService;
+        }
+
+        public IActionResult Index()
+        {
+          
+            return View(this._brandService.GetAll());
+        }
+    }
+}
