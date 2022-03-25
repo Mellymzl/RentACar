df9df9f64444b418662abca53a2161a83c7d62c3
melike yılmaz <melike_ylmz_61@hotmail.com>
2022-03-25 22:44:14 +0300

    car dto update

--- Business/Dtos/CarDto.cs
+++ Business/Dtos/CarDto.cs
@@ -14,5 +14,7 @@ namespace Business.Dtos
         public string Name { get; set; }
         public string Description { get; set; }
         public decimal DailyPrice { get; set; }
+        public int ColorId { get; set; }
+        public int BrandId { get; set; }
     }
 }
